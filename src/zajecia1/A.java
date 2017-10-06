eba3d55f7523d1789185a3ca43902ac4df6895b1
pmarecki <pmarecki@gmail.com>
2017-10-06 18:58:56 +0200

    Wypisywanie na ekran

--- src/zajecia1/A.java
+++ src/zajecia1/A.java
@@ -1,7 +1,15 @@
 package zajecia1;
 
+
 public class A {
+    //psvm + [tab]
     public static void main(String[] args) {
-        System.out.println("hello world");
+        //sout  +  [tab]
+        System.out.println("hello world!!");
+        System.out.println(2 + 3);       //[ctrl] + d
+
+        System.out.println("abra" + " " + "kadabra");
+
+        System.out.println("abra kadabra".toUpperCase());
     }
 }
