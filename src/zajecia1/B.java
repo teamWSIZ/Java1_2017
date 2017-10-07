0ded15f91d7c81415896993e78e4a2136e4ef36e
WSIZ <teamWSIZ@users.noreply.github.com>
2017-10-07 11:26:13 +0200

    Create B.java

---
+++ src/zajecia1/B.java
@@ -0,0 +1,44 @@
+package zajecia1;
+
+//github.com/teamWSIZ/Java1_2017
+public class B {
+
+    //psvm + {tab}
+    public static void main(String[] args) {
+        //sout + {tab}
+        System.out.println("Hello World!!");
+        System.out.println(2 + 17);
+
+        System.out.println("abra " + " !! " + "kadabra");
+
+        System.out.println( 5 * 5 * 5 * 5);
+
+        System.out.println( 5 + 3 * 10);
+
+        System.out.println("don pedro".toUpperCase());
+        System.out.println("abcba".length());
+
+        int x = 12;
+        System.out.println(x);
+
+        x = 123;
+        System.out.println(x);
+
+        int a = 10;
+        x = a * a;
+        System.out.println(x);
+        //zadanie: zadeklarować zmienne a i b, wpisać do nich liczby,
+        // i wypisać na ekran wyniki działań + - * / między a i b
+
+        String s = "Xi Jinping";
+        System.out.println(s + " - " +  s);
+
+        System.out.println(s.toUpperCase());
+        System.out.println(s.startsWith("Xi"));
+        System.out.println(s.startsWith("Yi"));
+        System.out.println("----");
+        System.out.println(s.contains("Jin"));
+
+
+    }
+}
