23a0a0b08f41c036bb994a876907aa9f117cbb45
Piotr Marecki <pmarecki.pro@gmail.com>
2017-10-14 10:43:03 +0200

    proste przykłady if-ów

---
+++ src/PAP.java
@@ -0,0 +1,7 @@
+public class PAP {
+    public static void main(String[] args) {
+        System.out.println();
+
+        //skróty: psvm , sout
+    }
+}
---
+++ src/zajecia2/B.java
@@ -0,0 +1,18 @@
+package zajecia2;
+
+public class B {
+    public static void main(String[] args) {
+
+        int x = 1;
+
+        if (x > 5) {
+            System.out.println("x jest > 5");
+            System.out.println("druga instrukcja");
+        } else {
+            System.out.println("x jest <=5");
+            System.out.println("inna instrukcja");
+        }
+
+        System.out.println("koniec programu");
+    }
+}
