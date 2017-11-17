1810b0ea96806f12ff7fc4fa1ddf23b069bb3f3a
pmarecki <pmarecki@gmail.com>
2017-11-17 18:37:12 +0100

    Prosty layout

--- src/zajecia6/Controller.java
+++ src/zajecia6/Controller.java
@@ -1,5 +1,19 @@
 package zajecia6;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+
 public class Controller {
+
+    @FXML
+    Canvas codecanvas;
+
+
+    public void rysujKod() {
+        GraphicsContext gc = codecanvas.getGraphicsContext2D();
+
+        gc.strokeLine(5,5, 250,350);
+    }
 
 }
