ea1edc7daca5389f5884ab8d014c73288b1eb917
pmarecki <pmarecki@gmail.com>
2017-11-18 10:27:36 +0100

    Początek szachownicy

--- src/zajecia6/bis/Controller.java
+++ src/zajecia6/bis/Controller.java
@@ -17,5 +17,12 @@ public class Controller {
         GraphicsContext gc = szachycanvas.getGraphicsContext2D();
         gc.setLineWidth(5);
         gc.strokeLine(5,5, 100,200);
+        gc.fillRect(50,50, 20, 20);
+        gc.fillRect(90,50, 20, 20);
+        gc.fillRect(70,70, 20, 20);
+
+        for (int i = 0; i < 10; i++) {
+            gc.fillRect(10 + i * 10,170, 5, 5);
+        }
     }
 }
