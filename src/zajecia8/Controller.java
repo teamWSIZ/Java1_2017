6436bf18ba934965b8c513c39af32763e2c9faa3
pmarecki <pmarecki@gmail.com>
2017-12-16 11:14:11 +0100

    Dodanie domyślnej daty i tekstu pod polem wyboru wyjazdu

--- src/zajecia8/Controller.java
+++ src/zajecia8/Controller.java
@@ -5,6 +5,10 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 
 public class Controller {
 
@@ -21,13 +25,32 @@ public class Controller {
     TextArea konsola;
 
     public void initialize() {
-
+        data_przyjazdu.setValue(LocalDate.now());
+        data_wyjazdu.setPromptText("data wyjazdu");
     }
 
 
     public void getIt() {
         String a = data_przyjazdu.getValue().toString();
+
+        LocalDate date = data_przyjazdu.getValue();
         konsola.appendText("Wybrano date przyjazdu: " + a);
+    }
+
+    public void showDaysBetween() {
+        LocalDate date1 = data_przyjazdu.getValue();
+        LocalDate date2 = data_wyjazdu.getValue();
+
+        if (date1==null) {
+            konsola.appendText("Uwaga -- data przyjazdu nie jest wybrana");
+        }
+
+        LocalDateTime datetime1 = date1.atStartOfDay();
+        LocalDateTime datetime2 = date2.atStartOfDay();
+
+        long daysBetween = Duration.between(datetime1, datetime2).toDays();
+        konsola.appendText("Dni pobytu: " + daysBetween);
+
     }
 
 
