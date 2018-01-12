d0149a64afac1df66619b9f650663ee62b35f88b
P Marecki <pmarecki.pro@gmail.com>
2018-01-12 18:51:27 +0100

    Wersja na else if

--- src/zajecia9/A.java
+++ src/zajecia9/A.java
@@ -2,7 +2,7 @@ package zajecia9;
 
 public class A {
     public static void main(String[] args) {
-        int k = 30;
+        int k = 20;
         int c = 7;
 
         //todo: gdy za mało
@@ -13,17 +13,17 @@ public class A {
         for (int i = 0; i < 100; i++) {
             int wydaje = 0;
 
-            if (doWydania>=1) {
-                wydaje = 1;
-            }
-            if (doWydania>=2) {
-                wydaje = 2;
-            }
-            if (doWydania>=5) {
-                wydaje = 5;
-            }
             if (doWydania>=10) {
                 wydaje = 10;
+            }
+            else if (doWydania>=5) {
+                wydaje = 5;
+            }
+            else if (doWydania>=2) {
+                wydaje = 2;
+            }
+            else if (doWydania>=1) {
+                wydaje = 1;
             }
 
             if (wydaje>0) {
--- src/zajecia9/Warunki.java
+++ src/zajecia9/Warunki.java
@@ -1,4 +1,18 @@
 package zajecia9;
 
 public class Warunki {
+    public static void main(String[] args) {
+
+        int x = 21;
+
+        if (x>20) {
+            System.out.println("duża liczba");
+        }
+        else if (x>10) {
+            System.out.println("średnia liczba");
+        }
+        else {
+            System.out.println("mała liczba");
+        }
+    }
 }
