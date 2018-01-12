fec11feab8533219399065d4965d882eaff4076a
P Marecki <pmarecki.pro@gmail.com>
2018-01-12 19:17:53 +0100

    Dodawanie monet

--- src/zajecia9/Controller.java
+++ src/zajecia9/Controller.java
@@ -11,6 +11,8 @@ public class Controller {
     @FXML
     ComboBox produkty;
 
+    int wrzuconaKwota = 0;
+
     //Ta funkcja jest uruchamiana zaraz na początku działania okna
     public void initialize() {
         //więcej: https://docs.oracle.com/javafx/2/ui_controls/combo-box.htm
@@ -19,10 +21,26 @@ public class Controller {
     }
 
     public void kup() {
-        System.out.println("do zaimplementwania");
 
         String wybrane = (String)produkty.getSelectionModel().getSelectedItem();
         ekran.appendText("Wybrano: " + wybrane + "\n");
+
+        int cena = 0;
+        if (wybrane=="Pomarańcza") {
+            cena = 10;
+        } else if (wybrane=="Jabłko") {
+            cena = 7;
+        } else if (wybrane=="Kiwi") {
+            cena = 15;
+        }
+
+        ekran.appendText("Cena:" + cena + "\n");
+
+    }
+
+    public void insterFive() {
+        wrzuconaKwota += 5;
+        ekran.appendText("Wrzucono 5; łącznie:" + wrzuconaKwota + "\n");
     }
 
 }
