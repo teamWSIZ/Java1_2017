b4b75ddf4eeb1336964005f1bc64828fdb9c7fef
P Marecki <pmarecki.pro@gmail.com>
2018-02-09 19:28:39 +0100

    Dodanie zmiany graczy po ruchu

--- src/zajecia_10/Controller.java
+++ src/zajecia_10/Controller.java
@@ -16,34 +16,65 @@ public class Controller {
 
     @FXML
     GridPane board;
+    @FXML
+    Button nextPlayerButton;
 
     int[][] b;
+    int nextPlayer = 1;
 
     //Ta funkcja jest uruchamiana zaraz na początku działania okna
     public void initialize() {
         b = new int[][]{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
+        //początkowa ikona następnego ruchu
+        nextPlayerButton.setGraphic(
+                new ImageView(new Image(getClass()
+                        .getResourceAsStream("kiwi.png"),
+                        75, 75, true, true)));
 
+        //to jest kod dodający przyciski tworzące planszę na GUI
         for (int r = 0; r < 3; r++) {
             for (int c = 0; c < 3; c++) {
+                //to się nazywa "programmatic" gui construction
                 Button button = new Button();
                 button.setGraphic(
                         new ImageView(new Image(getClass()
-                                .getResourceAsStream("banana.png"),
+                                .getResourceAsStream("blank.png"),
                                 75, 75, true, true)));
-                board.add(button, c, r );
+                board.add(button, c, r ); //tutaj dynamicznie dodajemy przycisk do GridPane
+
                 Integer rr = r;
                 Integer cc = c;
                 button.setOnAction(event -> {
                     System.out.println("Naciścięto button nr: " + rr + ", " + cc);
-                    button.setGraphic(new ImageView(new Image(getClass()
-                            .getResourceAsStream("kiwi.png"),
-                            75, 75, true, true)));
-                    b[rr][cc] = 1;
-                    for (int i = 0; i < 3; i++) {
-                        System.out.println(Arrays.toString(b[i]));
+                    if (b[rr][cc]!=0) return;   //jeśli pole już zajęte -- nie rób nic
+
+                    b[rr][cc] = nextPlayer;     //update modelu gry
+
+                    //ustawienie odpowiedniej ikony tam, gdzie kliknięto
+                    if (nextPlayer==1) {
+                        button.setGraphic(new ImageView(new Image(getClass()
+                                .getResourceAsStream("kiwi.png"),
+                                75, 75, true, true)));
+                    } else {
+                        button.setGraphic(new ImageView(new Image(getClass()
+                                .getResourceAsStream("banana.png"),
+                                75, 75, true, true)));
                     }
-                    System.out.println("----------");
+
+                    //update numeru następnego gracza
+                    if (nextPlayer==1) {
+                        nextPlayer = 2;
+                    } else {
+                        nextPlayer = 1;
+                    }
+
+                    //update ikony następnego gracza
+                    String nextPlayerIcon = "kiwi.png";
+                    if (nextPlayer==2) nextPlayerIcon = "banana.png";
+                    nextPlayerButton.setGraphic(new ImageView(new Image(getClass()
+                            .getResourceAsStream(nextPlayerIcon),
+                            75, 75, true, true)));
                 });
                 button.setOnMouseClicked(event -> {
                     if (event.getButton()== MouseButton.SECONDARY) {
@@ -52,9 +83,6 @@ public class Controller {
                 });
             }
         }
-
-//        button.setOnAction();
-
 
     }
 
