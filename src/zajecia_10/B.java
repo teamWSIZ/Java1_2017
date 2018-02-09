3d50690b19b1a298bdd1c19ffe577ca451ca2c4a
P Marecki <pmarecki.pro@gmail.com>
2018-02-09 18:50:03 +0100

    Sprawdzanie identyczności liczb w kolumnach

--- src/zajecia_10/B.java
+++ src/zajecia_10/B.java
@@ -35,10 +35,27 @@ public class B {
         int[][] d = new int[][]{{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
 
         d[1][2] = 13;   //przypisanie wartości w rzędzie "1", kolumnie "2" (licząc od zera)
-
+        d[2][0] = 1;
 
         for (int i = 0; i < 3; i++) {
             System.out.println(Arrays.toString(d[i]));
+        }
+
+        //zadanie: sprawdzić czy istnieje kolumna taka, że wszystkie liczby
+        //w tej kolumnie są takie same...
+
+        for (int kolumna = 0; kolumna < d[0].length; kolumna++) {
+            int szukana = d[0][kolumna];
+            int licznik = 0;
+            for (int rzad = 0; rzad < d.length; rzad++) {
+                if (d[rzad][kolumna]==szukana) {
+                    licznik++;
+                }
+            }
+            //sprawdzenie czy licznik jest dość duży...
+            if (licznik==d.length) {
+                System.out.println("Kolumna nr. " + kolumna + " jest OK");
+            }
         }
 
 
